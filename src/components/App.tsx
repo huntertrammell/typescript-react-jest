a6f065789ff65cccd4ca0e061edc3be4b2b32fa3
unknown <hunterttrammell@gmail.com>
2022-01-16 08:58:52 -0600

    [feature] Added in navigation

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import { User } from "../model/Model";
 import { AuthService } from "../services/AuthService";
+import {Router, Route, Switch} from 'react-router-dom';
+import history from '../utils/history';
+import { Navbar } from "./Navbar";
+import { Home } from "./Home";
+import { Login } from "./Login";
+import { Profile } from "./Profile";
 
-import {Login} from "../components/Login";
 
 interface AppState {
   user: User | undefined;
@@ -14,6 +19,9 @@ export class App extends React.Component<{}, AppState> {
   constructor(props: any){
     super(props)
     this.setUser = this.setUser.bind(this)
+    this.state = {
+      user: undefined
+    }
   }
 
   private setUser(user:User){
@@ -21,9 +29,22 @@ export class App extends React.Component<{}, AppState> {
     console.log(user)
   }
 
-  render(): React.ReactNode {
+  render() {
     return (
-        <div><Login authService={this.authService} setUser={this.setUser}/></div>
+        <div className="wrapper">
+          <Router history={history}>
+            <div>
+              <Navbar user={this.state.user}/>
+              <Switch>
+                  <Route exact path="/" component={Home} />
+                  <Route exact path="/login">
+                    <Login authService={this.authService} setUser={this.setUser} />
+                  </Route>
+                  <Route exact path="/profile" component={Profile} />
+              </Switch>
+            </div>
+          </Router>
+        </div>
     );
   }
 }
