cc120684a97b3376eb57206dad8552dfac52c4a7
unknown <hunterttrammell@gmail.com>
2022-01-16 07:48:37 -0600

    [feature] Create user interface

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,14 @@
+import React from 'react';
+import { User } from '../model/Model';
 
+interface AppState{
+  user: User
+}
 
-function App() {
-  return (
-   <div>Hello World</div>
-  );
+export class App extends React.Component<{}, {}>{
+  render(): React.ReactNode {
+      return <h1>App Works</h1>
+  }
 }
 
 export default App;
