37b97d2260aeffdb4aedfb6a7de48848fcacb632
unknown <hunterttrammell@gmail.com>
2022-01-16 07:53:28 -0600

    [feature] implemented authservce

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { User } from '../model/Model';
+import { AuthService } from '../model/Model'
 
 interface AppState{
   user: User | undefined
 }
 
 export class App extends React.Component<{}, AppState>{
+
+  //private authService: AuthService = new AuthService()
+
   render(): React.ReactNode {
       return <h1>App Works</h1>
   }
