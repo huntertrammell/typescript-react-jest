51d3be42516d1ac5cca0f74c3193652213451b5f
unknown <hunterttrammell@gmail.com>
2022-01-16 08:07:37 -0600

    [feature] Created login form

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { runInThisContext } from 'vm'
 import { AuthService } from '../services/AuthService'
 
 interface LoginProps {
@@ -12,11 +13,31 @@ interface LoginState {
     isLoggedIn: boolean
 }
 
-export class Login extends React.Component<LoginProps> {
+export class Login extends React.Component<LoginProps, LoginState> {
+
+    state: LoginState = {
+        userName: '',
+        password: '',
+        loginAttempted: false,
+        isLoggedIn: false
+    }
 
     render(): React.ReactNode {
         return (
-            <div>It Works</div>
+            <div>
+                <h2>Please Login</h2>
+                <form id="form-login">
+                    <div>
+                        <label htmlFor="userName">Username</label>
+                        <input type="text" name="userName" id="userName" value={this.state.userName} />
+                    </div>
+                    <div>
+                        <label htmlFor="password">Password</label>
+                        <input type="password" name="password" id="password" value={this.state.password} />
+                    </div>
+                    <button type="submit">Login</button>
+                </form>
+            </div>
         )
     }
 }
