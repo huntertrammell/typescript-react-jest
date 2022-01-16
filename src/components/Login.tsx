901af4ecd19895a546ea7caaba8d64fa08486b78
unknown <hunterttrammell@gmail.com>
2022-01-16 08:16:30 -0600

    [feature] added auth

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { SyntheticEvent } from 'react'
 import { runInThisContext } from 'vm'
 import { AuthService } from '../services/AuthService'
 
@@ -34,11 +34,21 @@ export class Login extends React.Component<LoginProps, LoginState> {
         this.setState({password: event.target.value})
     }
 
+    private async handleSubmit(event: SyntheticEvent){
+        event.preventDefault();
+        const result = await this.props.authService.login(this.state.userName, this.state.password)
+        if(result){
+            console.log(result)
+        } else {
+            console.log("wrong login")
+        }
+    }
+
     render(): React.ReactNode {
         return (
             <div>
                 <h2>Please Login</h2>
-                <form id="form-login">
+                <form id="form-login" onSubmit={e => this.handleSubmit(e)}>
                     <div>
                         <label htmlFor="userName">Username</label>
                         <input type="text" name="userName" id="userName" value={this.state.userName} onChange={e => this.setUserName(e)} />
